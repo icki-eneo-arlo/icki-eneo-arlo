a371f16015e6b06e2558825adbb4d102b483cd89
Tyler Paige <tylerclarkpaige@gmail.com>
2022-01-14 17:57:39 -0500

    Reset boilerplate to bare bones

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,8 +9,7 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
-import "./layout.css"
+import '../styles/index.css'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,25 +24,8 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+      <h1 className="visually-hidden">{data.site.siteMetadata.title}</h1>
+      <main>{children}</main>
     </>
   )
 }
